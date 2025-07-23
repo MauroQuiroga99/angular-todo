4e3ac8cd49ed32eb7b7da8776d74c449ad8e661e
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2025-07-23 18:42:08 -0500

    implementar routing

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-labs',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './labs.component.html',
-  styleUrl: './labs.component.css'
+  styleUrl: './labs.component.css',
 })
 export class LabsComponent {
-
+  welcome = 'Bienvenido a mi primer proyecto de Angular';
+  tasks = ['instalar angular', 'crear proyecto', 'crear componentes'];
 }
