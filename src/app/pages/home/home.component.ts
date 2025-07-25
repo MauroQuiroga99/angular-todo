c3c3fefdc3ccfa55a6d70394f09081581bb46a95
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2025-07-25 18:21:09 -0500

    Renderizar lista de tareas y hacerla dinámica con signals

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  tasks = signal(['instalar angular', 'crear proyecto', 'crear componentes']);
 
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.tasks.update((tasks) => [...tasks, newTask]);
+    input.value = '';
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) =>
+      tasks.filter((task, position) => position !== index)
+    );
+  }
 }
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +9,31 @@ import { CommonModule } from '@angular/common';
 })
 export class LabsComponent {
   welcome = 'Bienvenido a mi primer proyecto de Angular';
-  tasks = ['instalar angular', 'crear proyecto', 'crear componentes'];
+  tasks = signal(['instalar angular', 'crear proyecto', 'crear componentes']);
+  name = signal('Mauro Quiroga');
+  age = 25;
+  disabled = true;
+  img =
+    'https://preview.redd.it/gold-ship-aura-farming-v0-2lyyp35z3eef1.jpeg?width=1070&format=pjpg&auto=webp&s=9b1e4b1ec808bc40ff69cb02a9aee5f4f83237ae';
+  person = {
+    name: 'Mauro',
+    age: 25,
+    avatar:
+      'https://preview.redd.it/gold-ship-aura-farming-v0-2lyyp35z3eef1.jpeg?width=1070&format=pjpg&auto=webp&s=9b1e4b1ec808bc40ff69cb02a9aee5f4f83237ae',
+  };
+
+  clickHandler() {
+    alert('Hola, has hecho click en el botón');
+  }
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
+  }
+
+  keyDaownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
 }
